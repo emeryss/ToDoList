1fa4f3d40e94b85e75302a1024069e74df1f4e30
XLCOR <emeryss.steam@gmail.com>
2024-02-16 00:49:10 +0900

    third commit

--- main.js
+++ main.js
@@ -12,8 +12,24 @@
 
 let taskInput = document.getElementById("task-input");
 let addButton = document.getElementById("add-button");
+let tabs = document.querySelectorAll(".task-tabs div");
 let taskList = [];
+let mode = "all"
+let filterList = []
+
 addButton.addEventListener("click", addTask);
+taskInput.addEventListener("keydown", function(e){
+  if (e.keyCode == 13){
+    e.preventDefault(e);
+    addTask(e); // 왜 (e)를 넣어야 하는지???
+    console.log(e);
+  }
+})
+
+for(let i=1;i<tabs.length;i++){
+  tabs[i].addEventListener("click",function (event){
+    filter(event)})
+}
 
 function addTask() {
   let task = {
@@ -27,22 +43,32 @@ function addTask() {
 }
 
 function render() {
+  // 1. 내가 선택한 탭에 따라서
+  let list=[]
+  if (mode === "all"){
+    list = taskList
+  } else if (mode === "ongoing" || mode === "done"){
+    list = filterList
+  }
+  // 2. 리스트를 달리 보여준다
+  
+  
   let resultHTML = "";
-  for (let i = 0; i < taskList.length; i++) {
-    if (taskList[i].isComplete == true) {
+  for (let i = 0; i < list.length; i++) {
+    if (list[i].isComplete == true) {
       resultHTML += `<div class="task">
-        <div class="task-done">${taskList[i].taskContent}</div>
+        <div class="task-done">${list[i].taskContent}</div>
         <div>
-          <button onclick="toggleComplete('${taskList[i].id}')">Check</button>
-          <button onclick="deleteTask('${taskList[i].id}')">Delete</button>
+          <button onclick="toggleComplete('${list[i].id}')">Check</button>
+          <button onclick="deleteTask('${list[i].id}')">Delete</button>
         </div>
       </div>`;
     } else {
       resultHTML += `<div class="task">
-        <div>${taskList[i].taskContent}</div>
+        <div>${list[i].taskContent}</div>
         <div>
-          <button onclick="toggleComplete('${taskList[i].id}')">Check</button>
-          <button onclick="deleteTask('${taskList[i].id}')">Delete</button>
+          <button onclick="toggleComplete('${list[i].id}')">Check</button>
+          <button onclick="deleteTask('${list[i].id}')">Delete</button>
         </div>
       </div>`;
     }
@@ -74,8 +100,46 @@ function deleteTask(id){
   console.log(taskList)
 }
 
+function filter(event){
+  mode=event.target.id
+  filterList =[]
+  if(mode === "all"){
+    //전체 리스트를 보여준다
+    render()
+  } else if(mode === "ongoing"){
+    //진행중인 아이템을 보여준다
+    //task.isComplete==false가 진행중인 아이템이다
+    for(let i=0;i<taskList.length;i++){
+      if(taskList[i].isComplete === false){
+        filterList.push(taskList[i])
+      }
+    }
+    render()
+    console.log("진행중",filterList)
+  } else if (mode === "done"){
+    //끝나는 케이스
+    //task.isComplete==true
+    for(let i = 0;i<taskList.length;i++){
+      if(taskList[i].isComplete ===true){
+        filterList.push(taskList[i])
+      }
+    }
+    render()
+  }
+}
 
 function randomIDGenerate() {
   return "_" + Math.random().toString(36).substr(2, 9);
 }
 
+//underline
+let underLine = document.getElementById("under-line")
+let menus = document.querySelectorAll("div.task-tabs div")
+
+menus.forEach(menu=>menu.addEventListener("mouseover",(e)=>horizontalIndicator(e)))
+
+function horizontalIndicator(e){
+  underLine.style.left = e.currentTarget.offsetLeft + "px";
+  underLine.style.width = e.currentTarget.offsetWidth + "px";
+  underLine.style.top = e.currentTarget.offsetTop + e.currentTarget.offsetHeight + "px-1";
+}
